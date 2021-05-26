587ebd60d7fac3312ee5923dc4416c88c436e7cb
Leandro Ramos <leanduardo@hotmail.com>
2021-05-26 10:07:42 -0300

    Add cache time in api

--- pages/api/hello.tsx
+++ pages/api/hello.tsx
@@ -2,5 +2,9 @@
 import {NextApiRequest, NextApiResponse} from 'next'
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ name: 'John Doe' })
+  const date = new Date().toISOString();
+
+  res.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate');
+
+  res.status(200).json({ name: 'John Doe', date: date })
 }
